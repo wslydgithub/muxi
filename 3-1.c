c374e2d50cfffe1dd992d45a8b4e53a7bfe1b8a4
liangyadong <2771730573@qq.com>
2023-10-24 09:37:34 +0800

    3-1

---
+++ 3-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int A,B;
+	puts("请输入两个整数");
+	printf("整数A"); scanf("%d",&A);
+	printf("整数B"); scanf("%d",&B);
+	if (A%B)
+		puts("B不是A的约数");
+	else
+		puts("B是A的约数");
+	return 0;
+}
